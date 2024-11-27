78e39992f13c0d994a2f47c6e1bacfc652648b6a
Adrien Corenflos <adrien.corenflos@gmail.com>
2024-11-27 19:16:19 +0000

    Update notebooks with new JAX versions.

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@
 
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='pekf',
-    version='0.1',
-    description='Parallel Extended Kalman Filter.',
-    author='Adrien Corenflos',
-    author_email='adrien.corenflos@gmail.com',
-    url='https://github.com/AdrienCorenflos/parallelEKF',
+    name="pekf",
+    version="0.1",
+    description="Parallel Extended Kalman Filter.",
+    author="Adrien Corenflos",
+    author_email="adrien.corenflos@gmail.com",
+    url="https://github.com/AdrienCorenflos/parallelEKF",
     packages=find_packages(),
     install_requires=requirements,
 )
